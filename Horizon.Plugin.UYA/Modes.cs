5d738849f28c24c500d6d1203a6701285a3d32ad
Metroynome <yort124@gmail.com>
2023-10-22 22:30:57 -0400

    updates

--- Horizon.Plugin.UYA/Modes.cs
+++ Horizon.Plugin.UYA/Modes.cs
@@ -26,22 +26,7 @@ namespace Horizon.Plugin.UYA
         // custom mode ids
         // CMODE_ID_EXAMPLE = 1,
 
-        /*
-        CMODE_ID_GUN_GAME = 1,
-        CMODE_ID_INFECTED = CMODE_ID_GUN_GAME + 1,
-        CMODE_ID_INFINITE_CLIMBER = CMODE_ID_INFECTED + 1,
-        CMODE_ID_PAYLOAD = CMODE_ID_INFINITE_CLIMBER + 1,
-        CMODE_ID_SEARCH_AND_DESTROY = CMODE_ID_PAYLOAD + 1,
-        CMODE_ID_SURVIVAL = CMODE_ID_SEARCH_AND_DESTROY + 1,
-        CMODE_ID_THOUSAND_KILLS = CMODE_ID_SURVIVAL + 1,
-        CMODE_ID_GRIDIRON = CMODE_ID_THOUSAND_KILLS + 1,
-        CMODE_ID_TEAM_DEFENDER = CMODE_ID_GRIDIRON + 1,
-
         // reserved for custom maps
-        CMODE_ID_DUCK_HUNT = -1,
-        CMODE_ID_SPLEEF = -2,
-        CMODE_ID_HOVERBIKE_RACE = -3,
-        */
         CMODE_ID_SPLEEF = -1,
     }
 
